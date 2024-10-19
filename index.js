39e2bb1e58245da98bdf45c89546161055b6f4c0
Bhishan Sah <121842628+visionmax01@users.noreply.github.com>
2024-10-19 22:02:41 +0530

    update server more api added

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 // Allow all origins
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://rentoora.bhishansah.com.np', 
   credentials: true,
 }));
 
